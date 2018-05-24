d674e1fd0a1c94a838a4dc59aab39f975b9916e1
koraxiss <koraxis666@gmail.com>
2018-05-24 17:19:04 +0200

    added some documentation comments

--- src/main/scala/RBtree.scala
+++ src/main/scala/RBtree.scala
@@ -1,17 +1,35 @@
 /**
-  * Colors for Red Black tree nodes
+  * Colors for RBTree nodes
   */
-sealed abstract class Color
+sealed trait Color
 
 case object Red extends Color
 
 case object Black extends Color
 
+/**
+  *Red Black tree implementation
+  */
 class RBTree {
 
+  /**
+    * Abstract tree class used for Node and Empty case classes
+    * @tparam A Type of the value contained in RBtree
+    */
   abstract class Tree[+A]
 
-  case class Node[A](color: Color, left: Tree[A], value: A, right: Tree[A]) extends Tree[A]
-  case class Empty[A]() extends Tree[A]
+  /**
+    * Case class for representing not empty nodes of the RBTree
+    * @param color Color of the Node
+    * @param left Left subtree
+    * @param value Value of the node
+    * @param right Right subtree
+    */
+  final case class Node[A](color: Color, left: Tree[A], value: A, right: Tree[A]) extends Tree[A]
+
+  /**
+    * Case class for representing empty leaves of the RBTree
+    */
+  final case class Empty[A]() extends Tree[A]
 
 }
