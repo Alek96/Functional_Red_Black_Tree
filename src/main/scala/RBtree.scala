8e31e8630fd3ca0fb325eb4989b0c439f4be0b52
koraxiss <koraxis666@gmail.com>
2018-05-27 14:58:24 +0200

    added remove function

--- src/main/scala/RBtree.scala
+++ src/main/scala/RBtree.scala
@@ -39,14 +39,16 @@ class RBTree {
 
   /**
     * Function for inserting values into tree.
+    *
     * @param value Value which is to be inserted
-    * @param tree Tree in which we insert
+    * @param tree  Tree in which we insert
     * @tparam A Type of value
     * @return New tree with inserted value
     */
   def insert[A: Ordering](value: A, tree: Tree[A]): Tree[A] = {
     /**
       * Function chosing in which subtree to insert value
+      *
       * @param tree Tree in which we insert
       */
     def ins(tree: Tree[A]): Tree[A] = {
@@ -62,8 +64,9 @@ class RBTree {
 
     /**
       * Function for balancing tree after inserting
+      *
       * @param color Color of node we want to balance
-      * @param left Left subtree
+      * @param left  Left subtree
       * @param value Value of node
       * @param right Right subtree
       */
@@ -84,22 +87,121 @@ class RBTree {
 
     /**
       * FUnction for changing root to black
+      *
       * @param tree Tree which root we want to recolor
       * @return Tree with root recolored to black
       */
     def blacken(tree: Tree[A]): Tree[A] = {
       tree match {
         case Node(_, left, nodeValue, right) => Node(Black, left, nodeValue, right)
+        case _ => Empty
       }
     }
 
     blacken(ins(tree))
   }
 
+  final def remove[A: Ordering](value: A, tree: Tree[A]): Tree[A] = {
+    def balance(left: Tree[A], value: A, right: Tree[A]): Tree[A] = (left, right) match {
+      case (Node(Red, ll, lv, lr), Node(Red, rl, rv, rr)) =>
+        Node(Red, Node(Black, ll, lv, lr), value, Node(Black, rl, rv, rr))
+      case (Node(Red, Node(Red, lll, llv, llr), lv, lr), r) =>
+        Node(Red, Node(Black, lll, llv, llr), lv, Node(Black, lr, value, r))
+      case (Node(Red, ll, lv, Node(Red, lrl, lrv, lrr)), r) =>
+        Node(Red, Node(Black, ll, lv, lrl), lrv, Node(Black, lrr, value, r))
+      case (l, Node(Red, rl, rv, Node(Red, rrl, rrv, rrr))) =>
+        Node(Red, Node(Black, l, value, rl), rv, Node(Black, rrl, rrv, rrr))
+      case (l, Node(Red, Node(Red, rll, rlv, rlr), rv, rr)) =>
+        Node(Red, Node(Black, l, value, rll), rlv, Node(Black, rlr, rv, rr))
+      case (l, r) =>
+        Node(Black, l, value, r)
+    }
+
+    def balanceLeft(left: Tree[A], v: A, right: Tree[A]): Tree[A] = (left, right) match {
+      case (Node(Red, ll, lv, lr), r) =>
+        Node(Red, Node(Black, ll, lv, lr), v, r)
+      case (l, Node(Black, rl, rv, rr)) =>
+        balance(l, v, Node(Red, rl, rv, rr))
+      case (l, Node(Red, Node(Black, rll, rlv, rlr), rv, rr)) =>
+        Node(Red, Node(Black, l, v, rll), rlv, balance(rlr, rv, reden(rr)))
+      case _ => throw new Error("Something went wrong")
+    }
+
+    def balanceRight(left: Tree[A], v: A, right: Tree[A]): Tree[A] = (left, right) match {
+      case (l, Node(Red, rl, rv, rr)) =>
+        Node(Red, l, v, Node(Black, rl, rv, rr))
+      case (Node(Black, ll, lv, lr), r) =>
+        balance(Node(Red, ll, lv, lr), v, r)
+      case (Node(Red, ll, lv, Node(Black, lrl, lrv, lrr)), r) =>
+        Node(Red, balance(reden(ll), lv, lrl), lrv, Node(Black, lrr, v, r))
+      case _ => throw new Error("Something went wrong")
+    }
+
+    def removeLeft(c: Color, l: Tree[A], v: A, r: Tree[A], remVal: A): Tree[A] = {
+      (c, l, v, r) match {
+        case (Black, _, _, _) => balanceLeft(remove(remVal, l), v, r)
+        case (Red, _, _, _) => Node(Red, rem(remVal, l), v, r)
+      }
+    }
+
+    def removeRight(c: Color, l: Tree[A], v: A, r: Tree[A], remVal: A): Tree[A] = {
+      (c, l, v, r) match {
+        case (Black, _, _, _) => balanceRight(l, v, remove(remVal, r))
+        case (Red, _, _, _) => Node(Red, l, v, rem(remVal, r))
+      }
+    }
+
+    def add(left: Tree[A], right: Tree[A]): Tree[A] = (left, right) match {
+      case (Empty, r) => r
+      case (l, Empty) => l
+      case (Node(Red, ll, lv, lr), Node(Red, rl, rv, rr)) =>
+        add(lr, rl) match {
+          case Node(Red, a, va, b) => Node(Red, Node(Red, ll, lv, a), va,  Node(Red, b, rv, rr))
+          case c => Node(Red, ll, lv, Node(Red, c, rv, rr))
+        }
+      case (Node(Black, ll, xv, lr), Node(Black, rl, rv, rr)) =>
+        add(lr, rl) match {
+          case Node(Red, a, va, b) => Node(Red, Node(Red, ll, xv, a), va, Node(Black, b, rv, rr))
+          case c => balanceLeft(ll, xv, Node(Black, c, rv, rr))
+        }
+      case (l, Node(Red, rl, rv, rr)) => Node(Red, add(l, rl), rv, rr)
+      case (Node(Red, ll, lv, lr), r) => Node(Red, ll, lv, add(lr, r))
+      case _ => throw new Error("Something went wrong")
+    }
+
+
+    def blacken(tree: Tree[A]): Tree[A] = {
+      tree match {
+        case Node(_, left, nodeValue, right) => Node(Black, left, nodeValue, right)
+        case _ => Empty
+      }
+    }
+
+    def reden(tree: Tree[A]): Tree[A] = tree match {
+      case Node(Black, l, v, r) => Node(Red, l, v, r)
+      case _ => throw new Error("Expected black node in reden function")
+    }
+
+    def rem(value: A, tree: Tree[A]): Tree[A] = {
+      val newTree: Tree[A] = tree match {
+        case Empty => Empty
+        case Node(c, l, v, r) => {
+          if (value < v) removeLeft(c, l, v, r, value)
+          else if (v < value) removeRight(c, l, v, r, value)
+          else add(l, r)
+        }
+      }
+      newTree
+    }
+
+    blacken(rem(value, tree))
+  }
+
   /**
     * Function for checking if value exists in tree
+    *
     * @param value Value which existance we want to check
-    * @param tree Tree in which we look for it
+    * @param tree  Tree in which we look for it
     * @tparam A Type of Value
     * @return Result of search
     */
@@ -115,35 +217,36 @@ class RBTree {
     }
   }
 
-//  final def getList[A: Ordering](tree: Tree[A]): List[A] = {
-//    @tailrec
-//    def summed(tree: Tree[A], acc: List[A]): List[A] = {
-//      tree match {
-//        case Node(_, Empty, v, Empty) => summed(Empty, v :: acc)
-//        case Node(_, Empty, v, r) => summed(r, v :: acc)
-//        case Node(_, Node(_, Empty, lv, Empty), v, r) => summed(r, v :: lv :: acc)
-//        // rebuild branch to a simpler problem
-//        case Node(_, Node(_, ll, lv, lr), v, r) => summed(Node(Red, ll, lv, Node(Red, lr, v, r)), acc)
-//        case Empty => acc
-//      }
-//    }
-//
-//    summed(tree, List[A]())
-//  }
-//
-//  final def fromList[A: Ordering](list: List[A]): Tree[A] = {
-//    @tailrec
-//    def innerFromList(list: List[A], res: Tree[A] = Empty): Tree[A] = {
-//      if (list.isEmpty)
-//        res
-//      else
-//        innerFromList(list.tail, insert(list.head, res))
-//    }
-//
-//    innerFromList(list)
-//  }
+  //  final def getList[A: Ordering](tree: Tree[A]): List[A] = {
+  //    @tailrec
+  //    def summed(tree: Tree[A], acc: List[A]): List[A] = {
+  //      tree match {
+  //        case Node(_, Empty, v, Empty) => summed(Empty, v :: acc)
+  //        case Node(_, Empty, v, r) => summed(r, v :: acc)
+  //        case Node(_, Node(_, Empty, lv, Empty), v, r) => summed(r, v :: lv :: acc)
+  //        // rebuild branch to a simpler problem
+  //        case Node(_, Node(_, ll, lv, lr), v, r) => summed(Node(Red, ll, lv, Node(Red, lr, v, r)), acc)
+  //        case Empty => acc
+  //      }
+  //    }
+  //
+  //    summed(tree, List[A]())
+  //  }
+  //
+  //  final def fromList[A: Ordering](list: List[A]): Tree[A] = {
+  //    @tailrec
+  //    def innerFromList(list: List[A], res: Tree[A] = Empty): Tree[A] = {
+  //      if (list.isEmpty)
+  //        res
+  //      else
+  //        innerFromList(list.tail, insert(list.head, res))
+  //    }
+  //
+  //    innerFromList(list)
+  //  }
   /**
     * Function for creating an union of 2 trees
+    *
     * @param tree1 First tree
     * @param tree2 Second tree
     * @tparam A Type of value stored inside tree
@@ -158,6 +261,7 @@ class RBTree {
 
   /**
     * Function for intersecting 2 tree
+    *
     * @param tree1 First tree
     * @param tree2 Second tree
     * @tparam A Type of value
